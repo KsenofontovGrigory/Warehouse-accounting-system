20858378d50688bc7e485a8ac7c1b58a548a3da4
KsenofontovGrigory <gr.ksenofontov@it-justice.com>
2022-08-08 12:17:27 +0300

    Added ModalWindow

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -1,59 +1,154 @@
-import {useState} from 'react';
-import {Modal, Box, Button, TextField, Select, MenuItem} from "@mui/material";
+import { useEffect } from 'react';
+import {Modal, Box, Button, TextField, Select, MenuItem} from '@mui/material';
 
-import {style, textFieldStyle} from "./constants";
+import { InputWrapper } from '../InputWrapper';
+import { SelectWrapper } from './SelectWrapper/SelectWrapper';
+import { AddedProductsList } from './AddedProductsList';
 
+import { useSystemData } from '../../hooks/useSystemData';
+import { useModalWindowData } from './ModalWindow.utils';
+
+import {style, textFieldStyle} from './constants';
 import * as S from './ModalWindow.styles'
 
 export const ModalWindow = ({open, handleClose, title}) => {
-    const [warehouse, setWarehouse] = useState('');
+    const {
+        warehouses,
+        products,
+        addedProductsArr,
+    } = useSystemData()
 
-    const handleChange = (event) => {
-        setWarehouse(event.target.value);
-    };
+    const {
+        setQuantity,
+        warehouse,
+        handleChange,
+        setAddUnallocatedProducts,
+        addUnallocatedProducts,
+        setProductQuantity,
+        addWarehouses,
+        setName,
+        error,
+        addProducts,
+        setUnallocatedProducts,
+        unallocatedProducts,
+        selectProduct,
+        findProductName,
+        productName,
+        productQuantity,
+        handleChangeProductName,
+    } = useModalWindowData(handleClose)
+
+    const renderActionsName = (
+        <TextField
+            sx={textFieldStyle}
+            required
+            type='text'
+            inputProps={{maxLength: 20}}
+            onChange={(e) => setName(e.target.value)}
+        />
+    );
+
+    const renderActionsQuantity = (
+        <TextField
+            sx={textFieldStyle}
+            InputProps={{ inputProps: { min: 0 } }}
+            required
+            type='number'
+            onChange={(e) => {
+                if (+(e.target.value) < 1) {
+                    e.target.value = '1'
+                }
+                setQuantity(e.target.value)
+            }}
+        />
+    );
+
+    const renderActionsWarehouse = (
+        <Select
+            sx={textFieldStyle}
+            value={warehouse}
+            onChange={handleChange}
+        >
+            <MenuItem value=''>
+                <em>None</em>
+            </MenuItem>
+            {warehouses.map((item, index) => (
+                    <MenuItem
+                        value={item.name}
+                        key={index}
+                    >
+                        {item.name}
+                    </MenuItem>
+                )
+            )}
+        </Select>
+    );
+
+    useEffect(() => {
+        setUnallocatedProducts(products.filter((item) => item.warehouse === ''))
+    }, [products])
+
+    useEffect(() => {
+        !unallocatedProducts.length && setAddUnallocatedProducts(false)
+    }, [unallocatedProducts])
 
     return (
         <Modal
             open={open}
             onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
+            aria-labelledby='modal-modal-title'
+            aria-describedby='modal-modal-description'
         >
             <Box sx={style}>
                 <S.Title>
                     {title}
                 </S.Title>
-                <S.ContainerInput>
-                    <S.ProductTitle>Name</S.ProductTitle>
-                    <TextField
-                        sx={textFieldStyle}
-                        required
-                        type='text'
-                        inputProps={{maxLength: 20}}
-                    />
-                </S.ContainerInput>
-                <S.ContainerInput>
-                    <S.ProductTitle>Quantity</S.ProductTitle>
-                    <TextField
-                        sx={textFieldStyle}
-                        InputProps={{ inputProps: { min: 0, max: 20 } }}
-                        required
-                        type='number'
-                    />
-                </S.ContainerInput>
-                <S.ContainerInput>
-                    <S.ProductTitle>Warehouse</S.ProductTitle>
-                    <Select
-                        sx={textFieldStyle}
-                        value={warehouse}
-                        onChange={handleChange}
-                    >
-                        <MenuItem value={10}>Ten</MenuItem>
-                        <MenuItem value={20}>Twenty</MenuItem>
-                        <MenuItem value={30}>Thirty</MenuItem>
-                    </Select>
-                </S.ContainerInput>
-                <Button>Add</Button>
+                <InputWrapper
+                    title='Name'
+                    renderAction={renderActionsName}
+                />
+                {title === 'Add product' ? (
+                    <>
+                        <InputWrapper
+                            title='Quantity'
+                            renderAction={renderActionsQuantity}
+                        />
+                        <InputWrapper
+                            title='Warehouse'
+                            renderAction={renderActionsWarehouse}
+                        />
+                    </>
+                ) : (
+                    <>
+                        <>
+                            {addedProductsArr.map((item, index) => (
+                                <AddedProductsList
+                                    key={index}
+                                    item={item}
+                                    setUnallocatedProducts={setUnallocatedProducts}
+                                    unallocatedProducts={unallocatedProducts}
+                                />
+                            ))}
+                        </>
+                        {addUnallocatedProducts ? (
+                            <SelectWrapper
+                                setProductQuantity={setProductQuantity}
+                                productName={productName}
+                                handleChangeProductName={handleChangeProductName}
+                                unallocatedProducts={unallocatedProducts}
+                                findProductName={findProductName}
+                            />
+                        ) : null}
+                        <Button
+                            onClick={selectProduct}
+                            disabled={!unallocatedProducts.length || addUnallocatedProducts && (!productQuantity.length || !productName.length)}
+                        >
+                            Add unallocated products
+                        </Button>
+                    </>
+                )}
+                <Button onClick={title === 'Add product' ? addProducts : addWarehouses}>Add</Button>
+                <S.TextError>{error}</S.TextError>
             </Box>
         </Modal>
     );
--- src/components/ModalWindow/ModalWindow.styles.js
+++ src/components/ModalWindow/ModalWindow.styles.js
@@ -1,16 +1,11 @@
-import {styled} from "@mui/material";
+import {styled} from '@mui/material';
 
 export const Title = styled('div')({
     fontSize: '40px',
 })
 
-export const ContainerInput = styled('div')({
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    margin: '15px 0',
-})
-
-export const ProductTitle = styled('p')({
-    fontSize: '28px',
+export const TextError = styled('p')({
+    fontSize: '32px',
+    textAlign: 'center',
+    color: 'red',
 })
